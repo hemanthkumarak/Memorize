2c9afddf381dae0970eab5806788938f857739a5
A.k. Hemanth Kumar <hemanthkumar.ak14@gmail.com>
2022-06-17 23:44:21 +0800

    Resolving conflicts

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct EmojiMemoryGameContentView: View {
+struct ContentView: View {
     
     let viewModel: EmojiMemoryGame
     @State var emojiCount = 10
@@ -59,7 +59,7 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        EmojiMemoryGameContentView(viewModel: game)
+        ContentView(viewModel: game)
     }
 }
 
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -6,46 +6,14 @@
 //
 
 import SwiftUI
-import GoogleSignIn
-
-//@main
-//struct MemorizeApp: App {
-//    var body: some Scene {
-//        let game = EmojiMemoryGame()
-//        WindowGroup {
-//            EmojiMemoryGameContentView(viewModel: game)
-//
-//        }
-//    }
-//}
-
-import SwiftUI
-import GoogleSignIn
 
 @main
-struct DaysUntilBirthday: App {
-  @StateObject var authViewModel = AuthenticationViewModel()
-
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-        .environmentObject(authViewModel)
-        .onAppear {
-          GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-            if let user = user {
-              self.authViewModel.state = .signedIn(user)
-            } else if let error = error {
-              self.authViewModel.state = .signedOut
-              print("There was an error restoring the previous sign-in: \(error)")
-            } else {
-              self.authViewModel.state = .signedOut
-            }
-          }
-        }
-        .onOpenURL { url in
-          GIDSignIn.sharedInstance.handle(url)
+struct MemorizeApp: App {
+    var body: some Scene {
+        let game = EmojiMemoryGame()
+        WindowGroup {
+            ContentView(viewModel: game)
         }
     }
-  }
 }
 
